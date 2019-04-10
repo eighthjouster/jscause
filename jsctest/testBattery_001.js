329b329e02f41641f2574bdfb17830504f1346b9
Rafael <rafael@pachecopalencia.com>
2019-04-10 01:33:42 -0700

    More actual tests added; pt. 2.

--- jsctest/testBattery_001.js
+++ jsctest/testBattery_001.js
@@ -4,15 +4,6 @@ const test_001_emptyDir =
 {
   // only: true,
   testName: 'Empty app dir',
-  expectedLogMessages:
-  [
-    [ 'error', 'Cannot find jscause.conf file.' ],
-    [ 'error', 'Server not started.  No sites are running.' ]
-  ],
-  endOfExpectLogMessages:
-  [
-    [ 'error', 'Server not started.  No sites are running.' ]
-  ],
   onTestBeforeStart()
   {
     // Here we set up the test.  Config files, sample files, etc.
@@ -25,6 +16,15 @@ const test_001_emptyDir =
     // and right after all battery of tests are performed.
     this.doEmptyTestDirectory();
   },
+  expectedLogMessages:
+  [
+    [ 'error', 'Cannot find jscause.conf file.' ],
+    [ 'error', 'Server not started.  No sites are running.' ]
+  ],
+  endOfExpectLogMessages:
+  [
+    [ 'error', 'Server not started.  No sites are running.' ]
+  ],
   expectedLogMessagesPass()
   {
     // We got all the sequence of log messages we were expecting.
@@ -61,6 +61,11 @@ const test_002_emptyConfigFile =
 {
   // only: true,
   testName: 'Empty config file',
+  onTestBeforeStart()
+  {
+    console.log(`Starting test: ${this.testName}`);
+    this.createFile('jscause.conf', '');
+  },
   expectedLogMessages:
   [
     [ 'error', 'Server not started.  No sites are running.' ]
@@ -69,11 +74,6 @@ const test_002_emptyConfigFile =
   [
     [ 'error', 'Server not started.  No sites are running.' ]
   ],
-  onTestBeforeStart()
-  {
-    console.log(`Starting test: ${this.testName}`);
-    this.createFile('jscause.conf', '');
-  },
   expectedLogMessagesPass()
   {
     this.testPassed = true;
@@ -96,10 +96,15 @@ const test_002_emptyConfigFile =
   }
 };
 
-const test_003_emptyConfigFileWithBrackets =
+const test_003_configFileWithBrackets =
 {
   // only: true,
-  testName: 'Empty config file with brackets',
+  testName: 'Config file with brackets',
+  onTestBeforeStart()
+  {
+    console.log(`Starting test: ${this.testName}`);
+    this.createFile('jscause.conf', '{}');
+  },
   expectedLogMessages:
   [
     [ 'error', 'Server configuration:  The following configuration attribute was not found:' ],
@@ -110,11 +115,331 @@ const test_003_emptyConfigFileWithBrackets =
   [
     [ 'error', 'Server not started.  No sites are running.' ]
   ],
+  expectedLogMessagesPass()
+  {
+    this.testPassed = true;
+  },
+  expectedLogMessagesFail()
+  {
+    this.testPassed = false;
+  },
+  onServerStarted()
+  {
+    this.testPassed = false;
+    this.terminateApplication(/* 'The server started okay.  It might be good or bad, depending on the test.' */);
+  },
+  onServerError()
+  {
+  },
+  onTestEnd()
+  {
+    console.log(`Finished test: ${this.testName}`);
+  }
+};
+
+const test_004_emptyConfigFileSingleSpace =
+{
+  // only: true,
+  testName: 'Empty config file with just one single space',
   onTestBeforeStart()
   {
     console.log(`Starting test: ${this.testName}`);
-    this.createFile('jscause.conf', '{}');
+    this.createFile('jscause.conf', ' ');
   },
+  expectedLogMessages:
+  [
+    [ 'error', 'Invalid jscause.conf file format.' ],
+    [ 'error', 'Unexpected end of JSON input' ],
+    [ 'error', 'Server not started.  No sites are running.' ]
+  ],
+  endOfExpectLogMessages:
+  [
+    [ 'error', 'Server not started.  No sites are running.' ]
+  ],
+  expectedLogMessagesPass()
+  {
+    this.testPassed = true;
+  },
+  expectedLogMessagesFail()
+  {
+    this.testPassed = false;
+  },
+  onServerStarted()
+  {
+    this.testPassed = false;
+    this.terminateApplication(/* 'The server started okay.  It might be good or bad, depending on the test.' */);
+  },
+  onServerError()
+  {
+  },
+  onTestEnd()
+  {
+    console.log(`Finished test: ${this.testName}`);
+  }
+};
+
+const test_005_emptyConfigFileSingleNewLine =
+{
+  // only: true,
+  testName: 'Empty config file with just one single new line',
+  onTestBeforeStart()
+  {
+    console.log(`Starting test: ${this.testName}`);
+    this.createFile('jscause.conf', '\n');
+  },
+  expectedLogMessages:
+  [
+    [ 'error', 'Invalid jscause.conf file format.' ],
+    [ 'error', 'Unexpected end of JSON input' ],
+    [ 'error', 'Server not started.  No sites are running.' ]
+  ],
+  endOfExpectLogMessages:
+  [
+    [ 'error', 'Server not started.  No sites are running.' ]
+  ],
+  expectedLogMessagesPass()
+  {
+    this.testPassed = true;
+  },
+  expectedLogMessagesFail()
+  {
+    this.testPassed = false;
+  },
+  onServerStarted()
+  {
+    this.testPassed = false;
+    this.terminateApplication(/* 'The server started okay.  It might be good or bad, depending on the test.' */);
+  },
+  onServerError()
+  {
+  },
+  onTestEnd()
+  {
+    console.log(`Finished test: ${this.testName}`);
+  }
+};
+
+const test_006_configFile_p =
+{
+  // only: true,
+  testName: 'Config file with just the p letter',
+  onTestBeforeStart()
+  {
+    console.log(`Starting test: ${this.testName}`);
+    this.createFile('jscause.conf', 'p');
+  },
+  expectedLogMessages:
+  [
+    [ 'error', 'Invalid jscause.conf file format.' ],
+    [ 'error', 'Unexpected token p in JSON at position 0' ],
+    [ 'error', 'Server not started.  No sites are running.' ]
+  ],
+  endOfExpectLogMessages:
+  [
+    [ 'error', 'Server not started.  No sites are running.' ]
+  ],
+  expectedLogMessagesPass()
+  {
+    this.testPassed = true;
+  },
+  expectedLogMessagesFail()
+  {
+    this.testPassed = false;
+  },
+  onServerStarted()
+  {
+    this.testPassed = false;
+    this.terminateApplication(/* 'The server started okay.  It might be good or bad, depending on the test.' */);
+  },
+  onServerError()
+  {
+  },
+  onTestEnd()
+  {
+    console.log(`Finished test: ${this.testName}`);
+  }
+};
+
+const test_007_configFile_openingBracket =
+{
+  // only: true,
+  testName: 'Config file with just the opening curly bracket',
+  onTestBeforeStart()
+  {
+    console.log(`Starting test: ${this.testName}`);
+    this.createFile('jscause.conf', '{');
+  },
+  expectedLogMessages:
+  [
+    [ 'error', 'Invalid jscause.conf file format.' ],
+    [ 'error', 'Unexpected end of JSON input' ],
+    [ 'error', 'Server not started.  No sites are running.' ]
+  ],
+  endOfExpectLogMessages:
+  [
+    [ 'error', 'Server not started.  No sites are running.' ]
+  ],
+  expectedLogMessagesPass()
+  {
+    this.testPassed = true;
+  },
+  expectedLogMessagesFail()
+  {
+    this.testPassed = false;
+  },
+  onServerStarted()
+  {
+    this.testPassed = false;
+    this.terminateApplication(/* 'The server started okay.  It might be good or bad, depending on the test.' */);
+  },
+  onServerError()
+  {
+  },
+  onTestEnd()
+  {
+    console.log(`Finished test: ${this.testName}`);
+  }
+};
+
+const test_008_configFile_arrayOf1 =
+{
+  // only: true,
+  testName: 'Config file with just an array [1]',
+  onTestBeforeStart()
+  {
+    console.log(`Starting test: ${this.testName}`);
+    this.createFile('jscause.conf', '[1]');
+  },
+  expectedLogMessages:
+  [
+    [ 'error', '"0" is not a valid configuration key.' ],
+    [ 'error', 'Server not started.  No sites are running.' ]
+  ],
+  endOfExpectLogMessages:
+  [
+    [ 'error', 'Server not started.  No sites are running.' ]
+  ],
+  expectedLogMessagesPass()
+  {
+    this.testPassed = true;
+  },
+  expectedLogMessagesFail()
+  {
+    this.testPassed = false;
+  },
+  onServerStarted()
+  {
+    this.testPassed = false;
+    this.terminateApplication(/* 'The server started okay.  It might be good or bad, depending on the test.' */);
+  },
+  onServerError()
+  {
+  },
+  onTestEnd()
+  {
+    console.log(`Finished test: ${this.testName}`);
+  }
+};
+
+const test_009_configFile_singleInvalidKey =
+{
+  // only: true,
+  testName: 'Config file with just an unknown key',
+  onTestBeforeStart()
+  {
+    console.log(`Starting test: ${this.testName}`);
+    this.createFile('jscause.conf', '{\n  unknown: ""\n}\n');
+  },
+  expectedLogMessages:
+  [
+    [ 'error', 'Unexpected token u in JSON at position 2' ],
+    [ 'error', 'Server not started.  No sites are running.' ]
+  ],
+  endOfExpectLogMessages:
+  [
+    [ 'error', 'Server not started.  No sites are running.' ]
+  ],
+  expectedLogMessagesPass()
+  {
+    this.testPassed = true;
+  },
+  expectedLogMessagesFail()
+  {
+    this.testPassed = false;
+  },
+  onServerStarted()
+  {
+    this.testPassed = false;
+    this.terminateApplication(/* 'The server started okay.  It might be good or bad, depending on the test.' */);
+  },
+  onServerError()
+  {
+  },
+  onTestEnd()
+  {
+    console.log(`Finished test: ${this.testName}`);
+  }
+};
+
+const test_010_configFile_singleKeyInvalidVal =
+{
+  // only: true,
+  testName: 'Config file with unknown key and invalid value',
+  onTestBeforeStart()
+  {
+    console.log(`Starting test: ${this.testName}`);
+    this.createFile('jscause.conf', '{\n  "unknown": unknown\n}\n');
+  },
+  expectedLogMessages:
+  [
+    [ 'error', 'Unexpected token u in JSON at position 13' ],
+    [ 'error', 'Server not started.  No sites are running.' ]
+  ],
+  endOfExpectLogMessages:
+  [
+    [ 'error', 'Server not started.  No sites are running.' ]
+  ],
+  expectedLogMessagesPass()
+  {
+    this.testPassed = true;
+  },
+  expectedLogMessagesFail()
+  {
+    this.testPassed = false;
+  },
+  onServerStarted()
+  {
+    this.testPassed = false;
+    this.terminateApplication(/* 'The server started okay.  It might be good or bad, depending on the test.' */);
+  },
+  onServerError()
+  {
+  },
+  onTestEnd()
+  {
+    console.log(`Finished test: ${this.testName}`);
+  }
+};
+
+const test_011_configFile_singleUnknownKey =
+{
+  // only: true,
+  testName: 'Config file with unknown key',
+  onTestBeforeStart()
+  {
+    console.log(`Starting test: ${this.testName}`);
+    this.createFile('jscause.conf', '{\n  "unknown": ""\n}\n');
+  },
+  expectedLogMessages:
+  [
+    [ 'error', '"unknown" is not a valid configuration key.' ],
+    [ 'error', 'Check that all the keys and values in jscause.conf are valid.' ],
+    [ 'error', 'Server not started.  No sites are running.' ]
+  ],
+  endOfExpectLogMessages:
+  [
+    [ 'error', 'Server not started.  No sites are running.' ]
+  ],
   expectedLogMessagesPass()
   {
     this.testPassed = true;
@@ -141,5 +466,13 @@ const test_003_emptyConfigFileWithBrackets =
 module.exports = [
   test_001_emptyDir,
   test_002_emptyConfigFile,
-  test_003_emptyConfigFileWithBrackets
+  test_003_configFileWithBrackets,
+  test_004_emptyConfigFileSingleSpace,
+  test_005_emptyConfigFileSingleNewLine,
+  test_006_configFile_p,
+  test_007_configFile_openingBracket,
+  test_008_configFile_arrayOf1,
+  test_009_configFile_singleInvalidKey,
+  test_010_configFile_singleKeyInvalidVal,
+  test_011_configFile_singleUnknownKey
 ];
