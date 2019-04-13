a91e196c1212e5f15fcd32d5de27f400e0c4793b
Rafael <rafael@pachecopalencia.com>
2019-04-13 14:19:46 -0700

    More actual tests added; pt. 9.

--- jsctest/testBattery_002.js
+++ jsctest/testBattery_002.js
@@ -2,14 +2,14 @@
 
 const testUtils = require('./testBatteryUtils');
 
-// This battery depends on Test Batter 001 to pass in its entirety.
-
 const test_002_001_emptyLogsDirectory = Object.assign(testUtils.makeFromBaseTest('Empty logs dir'),
   {
     // only: true,
     onTestBeforeStart()
     {
       console.log(`Starting test: ${this.testName}`);
+      this.doEmptyTestDirectory();
+      this.createFile('jscause.conf', '{\n  "sites": [1],\n  "logging": {}\n}\n');
       this.doRemoveDirectoryFromPathList('logs', { ignoreIfMissing: true });
       this.doCreateDirectoryFromPathList(['logs']);
     },
@@ -31,7 +31,7 @@ const test_002_001_emptyLogsDirectory = Object.assign(testUtils.makeFromBaseTest
   }
 );
 
-const test_002_002_emptySiteConfig = Object.assign(testUtils.makeFromBaseTest('Empty site config'),
+const test_002_002_emptySiteConfigExtraComma = Object.assign(testUtils.makeFromBaseTest('Empty site config, extra comma'),
   {
     // only: true,
     onTestBeforeStart()
@@ -57,8 +57,142 @@ const test_002_002_emptySiteConfig = Object.assign(testUtils.makeFromBaseTest('E
   }
 );
 
+const test_002_003_emptySiteConfig = Object.assign(testUtils.makeFromBaseTest('Empty site config'),
+  {
+    // only: true,
+    onTestBeforeStart()
+    {
+      console.log(`Starting test: ${this.testName}`);
+      this.createFile('jscause.conf', '{\n  "sites": [\n    {\n    }\n  ],\n  "logging": {}\n}\n');
+    },
+    expectedLogMessages:
+    [
+      [ 'error', 'Site configuration: Missing name.' ],
+      [ 'error', 'Site (no name) not started.' ],
+      [ 'error', 'Server not started.  No sites are running.' ]
+    ],
+    endOfExpectLogMessages:
+    [
+      [ 'error', 'Server not started.  No sites are running.' ]
+    ],
+    onServerStarted()
+    {
+      this.testPassed = false;
+      this.terminateApplication(/* 'The server started okay.  It might be good or bad, depending on the test.' */);
+    }
+  }
+);
+
+const test_002_004_emptySiteName = Object.assign(testUtils.makeFromBaseTest('Site config, empty name'),
+  {
+    // only: true,
+    onTestBeforeStart()
+    {
+      console.log(`Starting test: ${this.testName}`);
+      this.createFile('jscause.conf', '{\n  "sites": [\n    {\n      "name": ""\n    }\n  ],\n  "logging": {}\n}\n');
+    },
+    expectedLogMessages:
+    [
+      [ 'error', 'Site configuration: Missing name.' ],
+      [ 'error', 'Site (no name) not started.' ],
+      [ 'error', 'Server not started.  No sites are running.' ]
+    ],
+    endOfExpectLogMessages:
+    [
+      [ 'error', 'Server not started.  No sites are running.' ]
+    ],
+    onServerStarted()
+    {
+      this.testPassed = false;
+      this.terminateApplication(/* 'The server started okay.  It might be good or bad, depending on the test.' */);
+    }
+  }
+);
+
+const test_002_005_missingSitePort = Object.assign(testUtils.makeFromBaseTest('Site config, missing port'),
+  {
+    // only: true,
+    onTestBeforeStart()
+    {
+      console.log(`Starting test: ${this.testName}`);
+      this.createFile('jscause.conf', '{\n  "sites": [\n    {\n      "name": "My Site"\n    }\n  ],\n  "logging": {}\n}\n');
+    },
+    expectedLogMessages:
+    [
+      [ 'error', 'Site configuration: Site name \'My Site\' is missing port.' ],
+      [ 'error', 'Site \'My Site\' not started.' ],
+      [ 'error', 'Server not started.  No sites are running.' ]
+    ],
+    endOfExpectLogMessages:
+    [
+      [ 'error', 'Server not started.  No sites are running.' ]
+    ],
+    onServerStarted()
+    {
+      this.testPassed = false;
+      this.terminateApplication(/* 'The server started okay.  It might be good or bad, depending on the test.' */);
+    }
+  }
+);
+
+const test_002_006_invalidSitePort = Object.assign(testUtils.makeFromBaseTest('Site config, invalid port'),
+  {
+    // only: true,
+    onTestBeforeStart()
+    {
+      console.log(`Starting test: ${this.testName}`);
+      this.createFile('jscause.conf', '{\n  "sites": [\n    {\n      "name": "My Site",\n      "port": ""\n    }\n  ],\n  "logging": {}\n}\n');
+    },
+    expectedLogMessages:
+    [
+      [ 'error', 'Site configuration:  Site name \'My Site\' has an invalid port.  Integer number expected.' ],
+      [ 'error', 'Site \'My Site\' not started.' ],
+      [ 'error', 'Server not started.  No sites are running.' ]
+    ],
+    endOfExpectLogMessages:
+    [
+      [ 'error', 'Server not started.  No sites are running.' ]
+    ],
+    onServerStarted()
+    {
+      this.testPassed = false;
+      this.terminateApplication(/* 'The server started okay.  It might be good or bad, depending on the test.' */);
+    }
+  }
+);
+
+const test_002_007_missingRootDirName = Object.assign(testUtils.makeFromBaseTest('Site config, missing root directory name'),
+  {
+    // only: true,
+    onTestBeforeStart()
+    {
+      console.log(`Starting test: ${this.testName}`);
+      this.createFile('jscause.conf', '{\n  "sites": [\n    {\n      "name": "My Site",\n      "port": 3000\n    }\n  ],\n  "logging": {}\n}\n');
+    },
+    expectedLogMessages:
+    [
+      [ 'error', 'Site configuration: Site name \'My Site\' is missing rootdirectoryname.' ],
+      [ 'error', 'Site \'My Site\' not started.' ],
+      [ 'error', 'Server not started.  No sites are running.' ]
+    ],
+    endOfExpectLogMessages:
+    [
+      [ 'error', 'Server not started.  No sites are running.' ]
+    ],
+    onServerStarted()
+    {
+      this.testPassed = false;
+      this.terminateApplication(/* 'The server started okay.  It might be good or bad, depending on the test.' */);
+    }
+  }
+);
 
 module.exports = [
   test_002_001_emptyLogsDirectory,
-  test_002_002_emptySiteConfig
+  test_002_002_emptySiteConfigExtraComma,
+  test_002_003_emptySiteConfig,
+  test_002_004_emptySiteName,
+  test_002_005_missingSitePort,
+  test_002_006_invalidSitePort,
+  test_002_007_missingRootDirName
 ];
