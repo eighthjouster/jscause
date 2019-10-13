bd5ee2cdc7396489594c1ff5da13e3488afe103e
Rafael <rafael@pachecopalencia.com>
2019-10-13 00:22:47 -0700

    Final set of plain request testing; get/post params up next.

--- jsctest/testBattery_contents_01.js
+++ jsctest/testBattery_contents_01.js
@@ -67,7 +67,7 @@ function makeTestEndBoilerplate()
 
 function areFlatArraysEqual(a, b)
 {
-  return a.every((line, i) => b[i] === line);
+  return a.every((line, i) => b[i] === line) && (a.length || !b.length);
 }
 
 function performTestRequestAndOutput({ onResponseEnd: onResponseEndCb })
@@ -310,11 +310,194 @@ const test_contents_006_jscp_index_special_symbols_in_strings = Object.assign(te
   }
 );
 
+const test_contents_007_jscp_index_multiple_output_tests = Object.assign(testUtils.makeFromBaseTest('Contents; JSCP file; index; mutiple output tests.'),
+  makeTestEndBoilerplate.call(this),
+  {
+    // only: true,
+    onTestBeforeStart()
+    {
+      this.createFile(['sites', 'mysite', 'website', 'index.jscp'],
+        [
+          '<js rt.print(\'1\'); /js><js rt.print(\'2\'); /js>',
+          '<js rt.print(\'3\'); /js>',
+          '<js rt.print(\'4\'); /js>',
+          '<js rt.print(\'5\');',
+          '/js>',
+          '<js',
+          'rt.print(\'6\'); /js>',
+          '<js',
+          'rt.print(\'7\');',
+          '/js>',
+          '',
+          '<js',
+          'rt.print(\'8\');/js><js',
+          '',
+          'rt.print(\'9\');',
+          '',
+          '/js>',
+          'abc<js rt.print(\'d\'); /js>',
+          '',
+          'efg',
+          '',
+          '<js rt.print(\'h\'); /js>',
+          '<js rt.print(\'i\');',
+          '',
+          '',
+          '/js><js',
+          '',
+          '',
+          'rt.print(\'j\');',
+          '',
+          '',
+          '/js>',
+          '<js               rt.print(\'k\');rt.print(\'l\');                   /js>',
+          '<JS rt.print(\'m\'); /JS>'
+        ].join('\n'));
+      this.isRequestsTest = true;
+    },
+    onReadyForRequests()
+    {
+      const { jscLib: { sanitizeForHTMLOutput } } = this;
+      processResponse.call(this, ({ dataReceived }) =>
+      {
+        const outputLines = dataReceived && (dataReceived.length === 1) && dataReceived[0].toString().split('\n') || [];
+        this.testPassed = (
+          areFlatArraysEqual(outputLines,
+            [
+              '12 ',
+              ' 3 ',
+              ' 4 ',
+              ' 5 ',
+              ' 6 ',
+              ' 7 ',
+              ' 89 ',
+              ' abcd ',
+              ' efg ',
+              ' h ',
+              ' ij ',
+              ' kl ',
+              ' m ',
+              ' '
+            ].map(sanitizeForHTMLOutput))
+        );
+      });
+    }
+  }
+);
+
+const test_contents_008_jscp_index_html_indicator = Object.assign(testUtils.makeFromBaseTest('Contents; JSCP file; index; initial html indicator.'),
+  makeTestEndBoilerplate.call(this),
+  {
+    // only: true,
+    onTestBeforeStart()
+    {
+      this.createFile(['sites', 'mysite', 'website', 'index.jscp'],
+        [
+          '<html/>Hello',
+          ''
+        ].join('\n'));
+      this.isRequestsTest = true;
+    },
+    onReadyForRequests()
+    {
+      const { jscLib: { sanitizeForHTMLOutput } } = this;
+      processResponse.call(this, ({ dataReceived }) =>
+      {
+        const outputLines = dataReceived && (dataReceived.length === 1) && dataReceived[0].toString().split('\n') || [];
+        this.testPassed = (
+          areFlatArraysEqual(outputLines,
+            [
+              ' ',
+              ' Hello ',
+              ' '
+            ].map(sanitizeForHTMLOutput))
+        );
+      });
+    }
+  }
+);
+
+const test_contents_009_jscp_index_html_indicator_pt2 = Object.assign(testUtils.makeFromBaseTest('Contents; JSCP file; index; initial html indicator, part 2.'),
+  makeTestEndBoilerplate.call(this),
+  {
+    // only: true,
+    onTestBeforeStart()
+    {
+      this.createFile(['sites', 'mysite', 'website', 'index.jscp'],
+        [
+          '<html/>',
+          'Hello',
+          ''
+        ].join('\n'));
+      this.isRequestsTest = true;
+    },
+    onReadyForRequests()
+    {
+      const { jscLib: { sanitizeForHTMLOutput } } = this;
+      processResponse.call(this, ({ dataReceived }) =>
+      {
+        const outputLines = dataReceived && (dataReceived.length === 1) && dataReceived[0].toString().split('\n') || [];
+        this.testPassed = (
+          areFlatArraysEqual(outputLines,
+            [
+              ' ',
+              ' Hello ',
+              ' '
+            ].map(sanitizeForHTMLOutput))
+        );
+      });
+    }
+  }
+);
+
+const test_contents_010_jscp_index_html_indicator_pt3 = Object.assign(testUtils.makeFromBaseTest('Contents; JSCP file; index; initial html indicator, part 3.'),
+  makeTestEndBoilerplate.call(this),
+  {
+    // only: true,
+    onTestBeforeStart()
+    {
+      initConsoleLogCapture();
+      this.createFile(['sites', 'mysite', 'website', 'index.jscp'],
+        [
+          '<html/>Hello',
+          '<js console.log(1);',
+          ''
+        ].join('\n'));
+      this.isRequestsTest = true;
+    },
+    onReadyForRequests()
+    {
+      const { jscLib: { sanitizeForHTMLOutput } } = this;
+      processResponse.call(this, ({ dataReceived, consoleLogOutput }) =>
+      {
+        const outputLines = dataReceived && (dataReceived.length === 1) && dataReceived[0].toString().split('\n') || [];
+        this.testPassed = (
+          (consoleLogOutput.status === 'captured') &&
+          areFlatArraysEqual(consoleLogOutput.lines,
+            [
+              1,
+            ]) &&
+          areFlatArraysEqual(outputLines,
+            [
+              ' ',
+              ' Hello ',
+              ' '
+            ].map(sanitizeForHTMLOutput))
+        );
+      });
+    }
+  }
+);
+
 module.exports = [
   test_contents_001_jscp_index_empty,
   test_contents_002_jscp_index_console_log,
   test_contents_003_jscp_index_rt_print,
   test_contents_004_jscp_index_syntax_error,
   test_contents_005_jscp_index_rt_print_pt2,
-  test_contents_006_jscp_index_special_symbols_in_strings
+  test_contents_006_jscp_index_special_symbols_in_strings,
+  test_contents_007_jscp_index_multiple_output_tests,
+  test_contents_008_jscp_index_html_indicator,
+  test_contents_009_jscp_index_html_indicator_pt2,
+  test_contents_010_jscp_index_html_indicator_pt3
 ];
