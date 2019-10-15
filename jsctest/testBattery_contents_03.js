f088c50fcb438b45be913cbf09db78d73005e2ee
Rafael <rafael@pachecopalencia.com>
2019-10-14 22:56:55 -0700

    Testing of forbidden form uploads.

--- jsctest/testBattery_contents_03.js
+++ jsctest/testBattery_contents_03.js
@@ -57,7 +57,7 @@ const
     areFlatArraysEqual
   } = testUtils;
 
-const test_contents_001_post_params_form = Object.assign(makeFromBaseTest('Contents; JSCP file; POST parameters; form'),
+const test_contents_001_post_params_form_pt1 = Object.assign(makeFromBaseTest('Contents; JSCP file; POST parameters; form; part 1; simple test'),
   makeTestEndBoilerplate.call(this),
   {
     // only: true,
@@ -77,6 +77,94 @@ const test_contents_001_post_params_form = Object.assign(makeFromBaseTest('Conte
       const jsCauseConfContents = makeBaseJsCauseConfContents();
       this.createFile('jscause.conf', JSON.stringify(jsCauseConfContents));
 
+      const siteConfContents = makeBaseSiteConfContents();
+      this.createFile(['sites', 'mysite', 'configuration', 'site.json'], JSON.stringify(siteConfContents));
+      
+      initConsoleLogCapture();
+      this.createFile(['sites', 'mysite', 'website', 'index.jscp'], 'console.log(rt.postParams[\'txt_name\']);');
+
+      this.isRequestsTest = true;
+    },
+    onReadyForRequests()
+    {
+      const postData = querystring.stringify(
+        {
+          'txt_name': 'some_name'
+        }
+      );
+
+      const postRequest = makeBaseRequest(
+        {
+          headers:
+          {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': Buffer.byteLength(postData)
+          }
+        }
+      );
+
+      processResponse(this, postRequest, ({ dataReceived, consoleLogOutput }) =>
+      {
+        this.testPassed = !dataReceived.length &&
+          (consoleLogOutput.status === 'captured') &&
+          areFlatArraysEqual(consoleLogOutput.lines,
+            [
+              'some_name'
+            ]);
+      }, postData);
+    }
+  }
+);
+
+const test_contents_002_post_params_form_pt2_forbidden = Object.assign(makeFromBaseTest('Contents; JSCP file; POST parameters; form; part 2; forbidden uploads'),
+  makeTestEndBoilerplate.call(this),
+  {
+    // only: true,
+    onTestBeforeStart()
+    {
+      const siteConfContents = makeBaseSiteConfContents(
+        {
+          'canUpload': false
+        }
+      );
+      this.createFile(['sites', 'mysite', 'configuration', 'site.json'], JSON.stringify(siteConfContents));
+
+      this.createFile(['sites', 'mysite', 'website', 'index.jscp'], '');
+
+      this.isRequestsTest = true;
+    },
+    onReadyForRequests()
+    {
+      const postData = querystring.stringify(
+        {
+          'txt_name': 'some_name'
+        }
+      );
+
+      const postRequest = makeBaseRequest(
+        {
+          headers:
+          {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': Buffer.byteLength(postData)
+          }
+        }
+      );
+
+      processResponse(this, postRequest, ({ statusCode }) =>
+      {
+        this.testPassed = (statusCode === 403);
+      }, postData);
+    }
+  }
+);
+
+const test_contents_003_post_params_form_pt3 = Object.assign(makeFromBaseTest('Contents; JSCP file; POST parameters; part 3; random values'),
+  makeTestEndBoilerplate.call(this),
+  {
+    // only: true,
+    onTestBeforeStart()
+    {
       const siteConfContents = makeBaseSiteConfContents();
       this.createFile(['sites', 'mysite', 'configuration', 'site.json'], JSON.stringify(siteConfContents));
       
@@ -114,7 +202,7 @@ const test_contents_001_post_params_form = Object.assign(makeFromBaseTest('Conte
           (consoleLogOutput.status === 'captured') &&
           areFlatArraysEqual(consoleLogOutput.lines,
             [
-              ['some_name', paramValue, 'submit'].join(' '),
+              ['some_name', paramValue, 'submit'].join(' ')
             ]);
       }, postData);
     }
@@ -122,5 +210,7 @@ const test_contents_001_post_params_form = Object.assign(makeFromBaseTest('Conte
 );
 
 module.exports = [
-  test_contents_001_post_params_form
+  test_contents_001_post_params_form_pt1,
+  test_contents_002_post_params_form_pt2_forbidden,
+  test_contents_003_post_params_form_pt3
 ];
